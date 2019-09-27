d9a40c94500507abc8bc783d26bc051b3de526ba
mickey english <mickeyeng92@gmail.com>
2019-09-27 16:30:57 +0100

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomPage';
+import Navbar from './components/Navbar';
+import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/articles-list" component={ArticlesList} />
+        <Route path="/article" component={ArticlePage} />
+      </div>
+    </Router>
   );
 }
 
